6c279d9e589558063cf1df47cade181cfcd922fc
ilhoon <rival6821@naver.com>
2024-01-18 22:52:39 +0900

    linux 추가

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const getImg = async (page, url) => {
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    // executablePath: '/usr/bin/chromium-browser'
+    executablePath: '/usr/bin/chromium-browser',
   });
   const page = await browser.newPage();
 
