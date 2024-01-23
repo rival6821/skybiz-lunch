6e3c6a4908a62c874e55ab33e256f6f4f3ec1f8c
이일훈 <rival6821@naver.com>
2024-01-23 09:48:59 +0900

    에러 처리

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
-const { default: axios } = require('axios');
-const moment = require('moment-timezone');
-const puppeteer = require('puppeteer');
+const { default: axios } = require("axios");
+const moment = require("moment-timezone");
+const puppeteer = require("puppeteer");
 
 const todayText = () => {
-  const text = moment().tz('Asia/Seoul').format('YYYY년 MM월 DD일');
-  return text;
-}
+    const text = moment().tz("Asia/Seoul").format("YYYY년 MM월 DD일");
+    return text;
+};
 
 const getImg = async (page, url) => {
-  await page.goto(url);
-  await page.waitForSelector('.card_cont .box_list_board a .item_thumb .wrap_fit_thumb', {timeout: 5000});
+    try {
+        await page.goto(url);
+        await page.waitForSelector(".card_cont .box_list_board a .item_thumb .wrap_fit_thumb", { timeout: 5000 });
 
-  const text = await page.evaluate(() => {
-    return document.querySelector(
-      '.box_list_board a .item_info .tit_info'
-    ).innerHTML;
-  });
+        const text = await page.evaluate(() => {
+            return document.querySelector(".box_list_board a .item_info .tit_info").innerHTML;
+        });
 
-  if (!text.includes(todayText())) {
-    return null;
-  }
+        if (!text.includes(todayText())) {
+            return null;
+        }
 
-  const image = await page.evaluate(() => {
-    return document.querySelector(
-      '.box_list_board a .item_thumb .wrap_fit_thumb'
-    ).style.backgroundImage;
-  });
+        const image = await page.evaluate(() => {
+            return document.querySelector(".box_list_board a .item_thumb .wrap_fit_thumb").style.backgroundImage;
+        });
+    } catch (error) {
+        return null;
+    }
 
-  return image.split('"')[1];
+    return image.split('"')[1];
 };
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: '/usr/bin/chromium-browser',
-  });
-  const page = await browser.newPage();
-
-  await page.setUserAgent(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'
-  );
-
-  const uncleImg = await getImg(page, 'https://pf.kakao.com/_FxbaQC'); // 삼촌밥차
-  const mouseImg = await getImg(page, 'https://pf.kakao.com/_CiVis/'); // 슈마우스
-
-  console.log(uncleImg, mouseImg);
-
-  if (uncleImg || mouseImg) {
-    const uploadUrl = 'https://lunch.muz.kr';
-    axios.post(uploadUrl, {
-      uncle: uncleImg,
-      mouse: mouseImg,
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: "/usr/bin/chromium-browser",
     });
-  }
+    const page = await browser.newPage();
 
-  await browser.close();
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0");
+
+    const uncleImg = await getImg(page, "https://pf.kakao.com/_FxbaQC"); // 삼촌밥차
+    const mouseImg = await getImg(page, "https://pf.kakao.com/_CiVis/"); // 슈마우스
+
+    console.log(uncleImg, mouseImg);
+
+    if (uncleImg || mouseImg) {
+        const uploadUrl = "https://lunch.muz.kr";
+        axios.post(uploadUrl, {
+            uncle: uncleImg,
+            mouse: mouseImg,
+        });
+    }
+
+    await browser.close();
 })();
