44641d08cda20eda25d0c31b77df45fcf6c38e4b
이일훈 <rival6821@naver.com>
2025-02-27 12:13:11 +0900

    index.js에서 콘솔 로그를 pino 로거로 변경 및 package.json에 pino 의존성 추가

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
 const moment = require("moment-timezone");
 const puppeteer = require("puppeteer");
+const logger = require('pino')();
 
 const todayText = moment().tz("Asia/Seoul").format("YYYY년 MM월 DD일");
 
 const getImg = async (page, url) => {
-    console.log(`Navigating to URL: ${url}`);
+    logger.info(`Navigating to URL: ${url}`);
     try {
         await page.goto(url);
         await page.waitForSelector(".card_cont .box_list_board a .item_thumb .wrap_fit_thumb", { timeout: 5000 });
@@ -14,10 +15,10 @@ const getImg = async (page, url) => {
             return document.querySelector(".box_list_board a .item_info .tit_info").innerHTML;
         });
 
-        console.log(`Fetched text: ${text}`);
+        logger.info(`Fetched text: ${text}`);
 
         if (!text.includes(todayText)) {
-            console.log(`Today's text not found in the content.`);
+            logger.info(`Today's text not found in the content.`);
             return null;
         }
 
@@ -25,16 +26,16 @@ const getImg = async (page, url) => {
             return document.querySelector(".box_list_board a .item_thumb .wrap_fit_thumb").style.backgroundImage;
         });
 
-        console.log(`Fetched image style: ${image}`);
+        logger.info(`Fetched image style: ${image}`);
         return image.split('"')[1];
     } catch (error) {
-        console.error(`Error in getImg: ${error.message}`);
+        logger.error(`Error in getImg: ${error.message}`);
         return null;
     }
 };
 
 const getPostImg = async (page, url) => {
-    console.log(`Navigating to URL: ${url}`); // 추가됨
+    logger.info(`Navigating to URL: ${url}`); // 추가됨
     try {
         await page.goto(url);
         await page.waitForSelector(".wrap_webview .area_card .wrap_archive_txt", {timeout: 5000});
@@ -42,35 +43,35 @@ const getPostImg = async (page, url) => {
         // 페이지 콘솔 로그를 Node.js 콘솔로 전달
         page.on('console', msg => {
             for (let i = 0; i < msg.args().length; ++i)
-                console.log(`PAGE LOG: ${msg.args()[i]}`);
+                logger.info(`PAGE LOG: ${msg.args()[i]}`);
         });
 
         const cards = await page.evaluate((todayText) => {
             const elements = document.querySelectorAll(".wrap_webview .area_card");
-            console.log(`Found ${elements.length} .area_card elements`);
+            logger.info(`Found ${elements.length} .area_card elements`);
             return Array.from(elements)
                 .filter(element => {
                     const title = element.querySelector(".tit_card")?.innerText || "";
                     const includesText = title.includes(todayText);
-                    console.log(`Title: ${title}, Includes todayText: ${includesText}`);
+                    logger.info(`Title: ${title}, Includes todayText: ${includesText}`);
                     // const desc = element.querySelector(".desc_card")?.innerHTML || "";
-                    // console.log(`Desc: ${desc}`);
+                    // logger.info(`Desc: ${desc}`);
                     // 제목 형태가 달라지는 경우가 고민임.
                     return includesText;
                 })
                 .map(element => {
                     const image = element.querySelector(".wrap_fit_thumb")?.style.backgroundImage;
-                    console.log(`Image : ${image}`);
+                    logger.info(`Image : ${image}`);
                     if (!image) return null;
                     return image.split('"')[1];
                 });
         }, todayText);
 
         const filteredCards = cards.filter(card => !!card);
-        console.log(`Extracted cards: ${filteredCards}`);
+        logger.info(`Extracted cards: ${filteredCards}`);
         return filteredCards ? filteredCards[0] : null;
     } catch (error) {
-        console.error(`Error in getPostImg: ${error.message}`);
+        logger.error(`Error in getPostImg: ${error.message}`);
         return null;
     }
 }
@@ -90,12 +91,10 @@ const workDoneCheck = async () => {
 
 (async () => {
     try {
-        console.log("=== RUN TIME : " + todayText + " ===");
-
         // 작업 필요유무 체크
         const workDoneResult = await workDoneCheck();
         if (workDoneResult) {
-            console.log('WORK DONE!');
+            logger.info('WORK DONE!');
             return;
         }
 
@@ -110,36 +109,36 @@ const workDoneCheck = async () => {
 
         await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0");
 
-        console.log('Fetching uncleImg...');
+        logger.info('Fetching uncleImg...');
         const uncleImg = await getImg(page, "https://pf.kakao.com/_FxbaQC"); // 삼촌밥차
 
-        console.log('Fetching mouseImg...');
+        logger.info('Fetching mouseImg...');
         const mouseImg = await getImg(page, "https://pf.kakao.com/_CiVis/"); // 슈마우스
 
-        console.log('Fetching jundamImg...');
+        logger.info('Fetching jundamImg...');
         const jundamImg = await getPostImg(page, "https://pf.kakao.com/_vKxgdn/posts"); // 정담
 
         if (uncleImg || mouseImg || jundamImg) {
-            console.log(`uncleImg: ${uncleImg}, mouseImg: ${mouseImg}, jundamImg: ${jundamImg}`);
+            logger.info(`uncleImg: ${uncleImg}, mouseImg: ${mouseImg}, jundamImg: ${jundamImg}`);
             const uploadUrl = "https://lunch.muz.kr";
             await axios.post(uploadUrl, {
                 uncle: uncleImg || '',
                 mouse: mouseImg || '',
                 jundam: jundamImg || '',
             });
-            console.log('Images uploaded successfully.');
+            logger.info('Images uploaded successfully.');
         } else {
-            console.log('No images to upload.');
+            logger.info('No images to upload.');
         }
 
         await browser.close();
-        console.log('Browser closed.');
+        logger.info('Browser closed.');
     } catch (error) {
-        console.error(`Error in main execution: ${error.message}`);
+        logger.error(`Error in main execution: ${error.message}`);
         // 브라우저가 열려 있다면 닫기
         if (browser) {
             await browser.close();
-            console.log('Browser closed due to error.');
+            logger.info('Browser closed due to error.');
         }
     }
 })();
